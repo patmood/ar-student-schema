cea68f2da16f338f8d99d4be9f7cf079f0f641ea
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-19 15:26:22 -0500

    inputs are validated

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,14 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, :presence => true, :uniqueness => true, :format => { :with => /^.+@.+\..{2,5}$/ , :message => 'Invalid email, bitch'}
+  validates :birthday, :presence => true
+  validates :age, :numericality => {:greater_than => 5}
+  validates :phone, :presence => true, :format => { :with => /\(\d{3}\).?\d{3}.?\d{4}/ , :message => 'Enter valid phone number'}
+
+
+
   def name
     first_name + " " + last_name
   end
@@ -10,4 +17,9 @@ class Student < ActiveRecord::Base
     now = Time.now.utc.to_date
     now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
   end
+
+  def age_greater_than_5
+   age > 5
+  end
+
 end
